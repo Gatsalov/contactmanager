921070f02ee32d43ee168ccbd5be121e8745d4ce
Alikhan Gatsalov <alihan_g5@mail.ru>
2019-04-28 00:09:56 +0300

    Updates

---
+++ precache-manifest.5985dc9f047f32b3cbae9d511417d90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e32d500ef9ec2700ec40",
+    "url": "/contactmanager/static/css/main.ab2d7cd3.chunk.css"
+  },
+  {
+    "revision": "e32d500ef9ec2700ec40",
+    "url": "/contactmanager/static/js/main.8372e055.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.4bdee9f6.js"
+  },
+  {
+    "revision": "2a030b5712a10864ccdc",
+    "url": "/contactmanager/static/css/2.2e331af4.chunk.css"
+  },
+  {
+    "revision": "2a030b5712a10864ccdc",
+    "url": "/contactmanager/static/js/2.d3e50fad.chunk.js"
+  },
+  {
+    "revision": "eda71741c6f16d18e495d60bca8666dd",
+    "url": "/contactmanager/index.html"
+  }
+];
